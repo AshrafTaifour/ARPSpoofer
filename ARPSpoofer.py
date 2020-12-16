7512801544291af534d02ac08e6351feeb58d920
Humzah Okadia <humzahokadia@Humzahs-MacBook-Pro.local>
2020-12-16 15:23:55 -0500

    allows ip routes/ ip forwarding for linux devices  <humzahokadia@Humzahs-MacBook-Pro.local>

--- ARPSpoofer.py
+++ ARPSpoofer.py
@@ -3,3 +3,12 @@ import argparse
 import time
 import os
 import sys
+
+def enable_linuxip():
+    #EnablesIP Forward in linux
+    filepath = "/proc/sys/net/ipv4/ip_forward"#linux os variable that allows the
+    with open(filepath) as f:                 #ip forwording to be turned on 
+        if f.read() == 1:                     #or off
+            return # 1 is already enabled and 0 means disabled
+    with open(filepath, "w") as f:
+        print(1, file=f)
